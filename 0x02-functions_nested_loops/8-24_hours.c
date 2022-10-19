11d4ff4c60f064ecc28b1269e98c36bd8780f63e
KelvinBaah <kelvinbaah54@gmail.com>
2022-10-19 10:27:52 -0700

    fixed 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 			_putchar('0');
 			_putchar(i + '0');
 			}
-			else if (i > 10)
+			else if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
@@ -28,11 +28,11 @@ void jack_bauer(void)
 			_putchar('0');
 			_putchar(j + '0');
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 		}
 	}
+	_putchar('\n');
 }
-
